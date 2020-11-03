7f21ad91b4cc38e6eece403a4dd16ca46ae9c449
Jinyoung <roghabo@gmail.com>
2020-11-03 16:57:57 +0900

    to video 9.4

--- redux/roomsSlice.js
+++ redux/roomsSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import api from "../api";
 
 const roomsSlice = createSlice({
   name: "rooms",
@@ -11,12 +12,35 @@ const roomsSlice = createSlice({
   },
   reducers: {
     setExploreRooms(state, action) {
-      state.explore.rooms.push(action.payload.rooms);
-      state.explore.page = action.payload.page;
+      const { explore } = state;
+      const { payload } = action;
+      payload.rooms.forEach((payloadRoom) => {
+        const exists = explore.rooms.find(
+          (savedRoom) => savedRoom.id === payloadRoom.id
+        );
+        if (!exists) {
+          explore.rooms.push(payloadRoom);
+        }
+      });
+      state.explore.page = payload.page;
     },
   },
 });
 
 const { setExploreRooms } = roomsSlice.actions;
+
+export const getRooms = () => async (dispatch) => {
+  try {
+    const {
+      data: { results },
+    } = await api.rooms();
+    dispatch(
+      setExploreRooms({
+        rooms: results,
+        page: 1,
+      })
+    );
+  } catch (e) {}
+};
 
 export default roomsSlice.reducer;
